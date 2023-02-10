b01a1fb92c7698d55f32c0b5478e06f24537b703
Mindy Barrs <mbarrs@deloitte.ca>
2023-02-10 11:40:02 -0500

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Barrs, Mindy on 2023-02-10.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
